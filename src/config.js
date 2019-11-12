eba4efe139c6ff58f93119130d7ecc02000e4c0e
zentco <colinsmail_7@hotmail.com>
2019-11-11 20:27:14 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "zentco-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://ova2qmk4ng.execute-api.us-east-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_Ba1vAKmZd",
+    APP_CLIENT_ID: "50g215rnenerm0q1t1kq25ubgk",
+    IDENTITY_POOL_ID: "us-east-2:8be13c46-506d-4edf-adaf-5151a8171b09"
+  }
+};
